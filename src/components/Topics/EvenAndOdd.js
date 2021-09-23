e44e9ae2a51c3ba7feaaa5a324af6c033831076b
Clint Edwards <cedwards1080@gmail.com>
2021-09-23 15:55:39 -0600

    Step 3 complete thanks jon

--- src/components/Topics/EvenAndOdd.js
+++ src/components/Topics/EvenAndOdd.js
@@ -2,12 +2,42 @@ import React, { useState } from "react";
 
 function EvenAndOdd() {
   const [evenArray, setEvenArray] = useState([]);
+  const [oddArray, setOddArray] = useState([]);
+  const [userInput, setUserInput] = useState("");
+
+  const handleChange = (e) => {
+    setUserInput(e.target.value);
+    console.log(userInput);
+  };
+
+  const numSplitter = (userInput) => {
+    let evenArr = [];
+    let oddArr = [];
+    userInput
+      .split(",")
+      .map((userInput) =>
+        userInput % 2 === 0 ? evenArr.push(userInput) : oddArr.push(userInput)
+      );
+    setEvenArray(evenArr);
+    setOddArray(oddArr);
+    console.log("hit");
+  };
+
   return (
     <div className="puzzleBox evenAndOddPB">
-      <input type="text" className="inputLine" />
-      <button className="confirmationButton"></button>
-      <span className="resultsBox"></span>
-      <span className="resultsBox"></span>
+      <h4>Evens and Odds</h4>
+      <input type="text" className="inputLine" onChange={handleChange} />
+      <button
+        className="confirmationButton"
+        onClick={() => {
+          numSplitter(userInput);
+        }}
+      >
+        Run
+      </button>
+      <span className="resultsBox">{evenArray}</span>
+      <span className="resultsBox">{oddArray}</span>
+      {/* {console.log(arraySplitter([]))} */}
     </div>
   );
 }
